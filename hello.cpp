d292beef8a6c1697902e210e557435bda1e33adb
Parth-RK <2021bcs119@sggs.ac.in>
2025-02-20 11:44:53 +0530

    Hello World

---
+++ hello.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+float a = 10;
+float b = 20;
+float d, e, f;
+
+cin >> d >> e;
+cin >> f;
+double c = (a + b + d * e)/f;
+
+cout << c;
+
+
+}
